6015d6debcb543fe36a948f3d25028761b4068fd
Fingerlingus <sirfingerlingus@gmail.com>
2022-04-27 01:04:42 -0600

    create kheap_begin(), gut kmalloc() and kfree()

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,25 +1,38 @@
-#include "memory.hpp"
-
 #include <cstdint>
 #include <new>
+
+#include "stdlib/list.hpp"
+#include "stdlib/expected.hpp"
+
+#include "memory.hpp"
+#include "page_table.hpp"
 
 #ifndef NO_DISCARD
 #	define NO_DISCARD [[nodiscard]]
 #endif
+
+bool kheap_init() {
+    std::size_t num_pages = KHEAP_SIZE / 4096;
+    if(KHEAP_SIZE % 4096 != 0) num_pages++;
+
+    char* p = (char*)KHEAP_BEGIN;
+    while(p != KHEAP_END) {
+        if(pt->alloc_page(p) == nullptr)
+            return false;
+    }
+    return true;
+}
 
 #ifdef __cplusplus
 	extern "C" {
 #endif
 
 NO_DISCARD void* kmalloc(std::size_t size) {
-	static char* free_memory_base = (char*)HEAP_BASE;
-	size = (size + 7) / 8 * 8;
-	free_memory_base += size;
-	return free_memory_base - size;
+    return nullptr;
 }
 
-void kfree(void*) {
-    return;
+void kfree(void* ptr) {
+
 }
 
 #ifdef __cplusplus
