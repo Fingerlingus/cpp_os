325360a695136a8754b1bf7ce8bbd9454fa52d79
Fingerlingus <sirfingerlingus@gmail.com>
2022-04-27 00:56:36 -0600

    pass script arguments to qemu

--- src/qemu.sh
+++ src/qemu.sh
@@ -1,1 +1,6 @@
-qemu-system-x86_64 -bios /usr/share/ovmf/x64/OVMF.fd -drive id=disk,file=image.hdd,if=none -device ahci,id=ahci -device ide-hd,drive=disk,bus=ahci.0 -m 512M
+qemu-system-x86_64 \
+    -bios /usr/share/ovmf/x64/OVMF.fd \
+    -drive id=disk,file=image.hdd,if=none \
+    -device ahci,id=ahci \
+    -device ide-hd,drive=disk,bus=ahci.0 -m 256M \
+    "$@"
